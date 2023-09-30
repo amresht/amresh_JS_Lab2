8a90b1ba93caf205884ec507a5d6ef13c8be3b26
Amresh Tripathi <amresh2k@gmail.com>
2023-09-30 23:11:08 +0530

    ver 1.0

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+
+// Questions that will be asked
+const Questions = [{
+	q: "What is capital of India?",
+	ans: "Delhi",
+	a: [{ text: "Gandhinagar", isCorrect: false },
+	{ text: "Surat", isCorrect: false },
+	{ text: "Delhi", isCorrect: true },
+	{ text: "Mumbai", isCorrect: false }
+	]
+},
+{
+	q: "What is the capital of Thailand?",
+	ans: "Bangkok",
+	a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+	{ text: "Phuket", isCorrect: false },
+	{ text: "Ayutthaya", isCorrect: false },
+	{ text: "Bangkok", isCorrect: true }
+	]
+
+},
+{
+	q: "What is the capital of Gujarat",
+	ans: "Gandhinagar",
+	a: [{ text: "Surat", isCorrect: false },
+	{ text: "Vadodara", isCorrect: false },
+	{ text: "Gandhinagar", isCorrect: true },
+	{ text: "Rajkot", isCorrect: false }
+	]
+
+}
+
+];
+
+let currQuestion = 0;
+let score = 0;
+
+let numQues =  Questions.length;
+const progress = document.getElementById("progress");
+
+function loadQues() {
+	const ques = document.getElementById("question");
+	ques.innerText = Questions[currQuestion].q;
+
+	const btnLabel0 = document.getElementById("choice0");
+	const btnLabel1 = document.getElementById("choice1");
+	const btnLabel2 = document.getElementById("choice2");
+	const btnLabel3 = document.getElementById("choice3");
+
+	btnLabel0.innerHTML = Questions[currQuestion].a[0].text;
+	//alert(Questions[currQuestion].a[0].text);
+	btnLabel1.innerHTML = Questions[currQuestion].a[1].text;
+	btnLabel2.innerHTML = Questions[currQuestion].a[2].text;
+	btnLabel3.innerHTML = Questions[currQuestion].a[3].text;
+	
+	tmp = currQuestion + 1;
+	progress.innerText="Question " + tmp +  " of " + numQues;
+
+
+}
+
+loadQues();
+
+function loadScore() {
+	const totalScore = document.getElementById("score")
+	totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+
+
+function nextQuestion() {
+	if (currQuestion < Questions.length - 1) {
+		currQuestion++;
+		loadQues();
+	} else {
+		const quiz = document.getElementById("quiz");
+		quiz.hidden=true;
+		loadScore();
+	}
+}
+
+function checkAns(choicez) {
+	const selectedAns = document.getElementById(choicez).innerHTML;
+
+	if (selectedAns == Questions[currQuestion].ans) {		
+		score++;
+		console.log("Correct")
+	}
+	nextQuestion();
+
+}
